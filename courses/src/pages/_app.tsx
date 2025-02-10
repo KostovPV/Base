f2424db4584cf9ca3245e77ce638be6853a52d7f
KostovPV <106186518+KostovPV@users.noreply.github.com>
2025-02-10 09:23:29 +0200

    Course Componet added, and global styles implemented

--- courses/src/pages/_app.tsx
+++ courses/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { I18nProvider } from 'next-localization';
 import NProgress from 'nprogress';
 import { SitecorePageProps } from 'lib/page-props';
 import Bootstrap from 'src/Bootstrap';
-
+import '../styles/globals.scss';
 // Using bootstrap and nprogress are completely optional.
 //  bootstrap is used here to provide a clean layout for samples, without needing extra CSS in the sample app
 //  nprogress provides a loading indicator on page/route changes
